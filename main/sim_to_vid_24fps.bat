97474c936f9bc5774f783fae2627c1497221b970
SuperCat908809 <supercat908809@gmail.com>
2024-11-22 23:29:47 +0000

    Tweaked renders to video batch file.

--- main/sim_to_vid_24fps.bat
+++ main/sim_to_vid_24fps.bat
@@ -1,3 +1,3 @@
 @echo off
 
-ffmpeg -y -r 24 -start_number 0 -i "sim\waves %%06d.jpg" -vf scale=-1:1080:flags=neighbor -c:v libx264 -crf 24 -r 24 out.mp4
+ffmpeg -y -r 24 -start_number 0 -i "sim\iter %%06d.png" -vf scale=-1:1080:flags=neighbor -c:v libx264 -crf 32 -r 24 out.mp4
